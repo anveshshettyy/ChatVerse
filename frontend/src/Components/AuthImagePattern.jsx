02b512e15c7c4c155f2d9464bc773505e2ef7d39
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-02-01 12:48:49 +0530

    final project

--- frontend/src/Components/AuthImagePattern.jsx
+++ frontend/src/Components/AuthImagePattern.jsx
@@ -1,6 +1,6 @@
 const AuthImagePattern = () => {
   return (
-    <div className="  flex items-center justify-center p-20">
+    <div className="  flex items-center justify-center p-20 bg-transparent">
       <h1 className="text-[3vh] text-center">Welcome to ChatVerse – a real-time messaging platform built with MERN stack by Anvesh Shetty. Connect with others and exchange messages in real time with a simple and easy-to-use interface."</h1>
     </div>
   )
